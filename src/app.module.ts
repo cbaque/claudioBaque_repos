5b8636dc7f6241ba9924d2cf960da57a5c6fb452
cbaque <claudio.sys16@gmail.com>
2022-08-14 15:32:27 -0500

    install typeorm with connect cluster postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type : 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      ssl: true,
+      extra: {
+        options: process.env.DB_CLUSTER,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
+
