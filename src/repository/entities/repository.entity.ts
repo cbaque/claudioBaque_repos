10ad6efcaaf667b2156f4a464c9a5b73dbc822d5
cbaque <claudio.sys16@gmail.com>
2022-08-14 21:43:34 -0500

    entitie repository ok

--- src/repository/entities/repository.entity.ts
+++ src/repository/entities/repository.entity.ts
@@ -1,10 +1,14 @@
-import { Column, PrimaryGeneratedColumn, Timestamp } from "typeorm";
+import { Tribe } from "src/tribe/entities/tribe.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, Timestamp } from "typeorm";
 
+@Entity()
 export class Repository {
     @PrimaryGeneratedColumn('identity')
     id_repository: number;
 
-    @Column('int', { nullable: false })
+    @OneToOne(() => Tribe)
+    @JoinColumn()
+    @Column('int')
     id_tribe: number    
 
     @Column('char', { length: 50, nullable: false })
@@ -13,9 +17,9 @@ export class Repository {
     @Column('char', { length: 1, nullable: false })
     state: string
     
-    @Column('timestamp', {  nullable: false })
-    create_time: Timestamp   
+    @CreateDateColumn()
+    create_time: Date   
     
-    @Column('char', { length: 1, nullable: false })
-    status: number    
+    @Column('char', { length: 1, nullable: false, default: 'A' })
+    status: string    
 }
